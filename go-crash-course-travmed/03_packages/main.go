bddbe449e6b7126f6f4546182bc49a22f9f66361
Rob Q <robq@Robs-MacBook-Pro.local>
2023-04-23 21:25:08 +0100

    got strutil package working

--- go-crash-course-travmed/03_packages/main.go
+++ go-crash-course-travmed/03_packages/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/Quackers71/go/go-crash-course-travmed/03_packages/strutil"
 	"math"
 )
 
@@ -9,4 +10,6 @@ func main() {
 	fmt.Println(math.Floor(2.7))
 	fmt.Println(math.Ceil(2.7))
 	fmt.Println(math.Sqrt(16))
+
+	fmt.Println(strutil.Reverse("olleH"))
 }
